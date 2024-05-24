c7ec655b9b558595a831b0d082028ade72412365
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-05-24 14:50:49 -0300

    :sparkles: Feat: componente Text

---
+++ src/components/QRCode/components/Text/Text.tsx
@@ -0,0 +1,25 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+type TextProps = {
+    text: string
+}
+
+const Text = (props: TextProps) => {
+    const { text } = props
+  return (
+    <Typography
+        variant="h6"
+        sx={{
+            color: 'hsl(218, 44%, 22%)',
+            fontWeight: 'bold',
+            textAlign: 'center',
+            marginTop: 4,
+        }}
+    >
+        {text}
+    </Typography>
+  )
+}
+
+export default Text
